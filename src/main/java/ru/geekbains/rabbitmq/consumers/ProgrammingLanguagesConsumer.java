3a7b55ff7edf2136a4e94528329d8ae8c2c16119
Николай <firewather@gmail.com>
2022-03-27 14:17:36 +0300

    example

---
+++ src/main/java/ru/geekbains/rabbitmq/consumers/ProgrammingLanguagesConsumer.java
@@ -0,0 +1,63 @@
+package ru.geekbains.rabbitmq.consumers;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+public class ProgrammingLanguagesConsumer {
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Print exchanger name to connect");
+        String exchangerName = in.nextLine();
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setConnectionTimeout(10000);
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(exchangerName, BuiltinExchangeType.DIRECT);
+        String queueName = channel.queueDeclare().getQueue();
+
+        List<String> deliveryThemes = new ArrayList<>();
+
+        System.out.println("Print themes to deliver and 'end' to exit binding");
+        String inputTheme;
+
+        while (in.hasNextLine() && !((inputTheme = in.nextLine()).equalsIgnoreCase("end"))) {
+            deliveryThemes.add(inputTheme);
+        }
+
+        for (String themes : deliveryThemes) {
+            channel.queueBind(queueName, exchangerName, themes);
+        }
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(message);
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+
+        Thread unbinding = new Thread(() -> {
+            String unbindingTheme;
+            while (true) {
+                if (in.hasNextLine() && !((unbindingTheme = in.nextLine()).substring(0, 5).equalsIgnoreCase("unbind"))) {
+                    try {
+                        channel.queueUnbind(queueName, exchangerName, unbindingTheme.substring(7));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        unbinding.start();
+    }
+}
---
+++ src/main/java/ru/geekbains/rabbitmq/producers/ProgrammingLanguagesProducer.java
@@ -0,0 +1,31 @@
+package ru.geekbains.rabbitmq.producers;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class ProgrammingLanguagesProducer {
+    private static final String EXCHANGER_NAME = "Programming";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setConnectionTimeout(10000);
+
+        try (Connection connection = connectionFactory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(EXCHANGER_NAME, BuiltinExchangeType.DIRECT);
+
+            channel.basicPublish(EXCHANGER_NAME, "php", null, "PHP MSG".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGER_NAME, "java", null, "JAVA MSG".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGER_NAME, "python", null, "PYTHON MSG".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGER_NAME, "c++", null, "C++ MSG".getBytes(StandardCharsets.UTF_8));
+        }
+
+    }
+}
